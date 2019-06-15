561ad75d29e71fd5f09c5fc0e0a6103a52d5dfd5
evangelineraj <sweetyraj96@gmail.com>
2019-06-15 23:43:26 +0530

    new project

---
+++ src/sample/CourseDetails.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class CourseDetails {
+	
+public void JavaCourse()
+{
+	System.out.println("Java Course started");
+}
+public void OracleCourse()
+{
+	System.out.println("oracle course started");
+}
+  public static void main(String[] args) {
+	  
+		  CourseDetails  ds = new CourseDetails();
+		  ds.JavaCourse();
+		  ds.OracleCourse();
+		  
+	  }
+	  }  
+
